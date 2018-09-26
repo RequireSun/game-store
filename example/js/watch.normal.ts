956c251f71f6125069ea4ea51d9354513822330e
kelvinsun <kelvinsun@tencent.com>
2018-09-26 19:22:32 +0800

    绝望 istanbul，改用 jest 去

---
+++ example/js/watch.normal.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+import GameStore from '../../src/index.ts';
+const assert = require('assert');
+
+export const run: () => GameStore = () => {
+    const instance = new GameStore({
+        state: {
+            a: 1,
+        },
+    });
+
+    instance.watch('a', (val, oldVal) => {
+        console.log('a', val, oldVal);
+
+        assert.equal(val, 2, 'a');
+        assert.equal(oldVal, 1, 'a');
+    });
+
+    setTimeout(() => {
+        instance.a = 2;
+    }, 20);
+
+    return instance;
+};
